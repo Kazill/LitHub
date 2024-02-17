10c9b85791bb31df1e364b9ede5567ea73df5098
Neda <neda.ramanauskaite@ktu.edu>
2024-02-17 18:17:13 +0200

    routes to different pages

---
+++ lithub/src/project.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+        function Project() {
+        return (
+<div>
+    <center><h1>Project name</h1></center>
+</div>
+        );
+        }
+        export default Project;
