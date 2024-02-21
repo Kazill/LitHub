ee4a7b0ce2bd592ae95d90087d153523a4bbe441
MartinBin <126594886+MartinBin@users.noreply.github.com>
2024-02-21 19:11:49 +0200

    Added an example

---
+++ BackEnd/Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using BackEnd.Data;
+using BackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly LithubContext _context;
+        public UserController(LithubContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<UserController>
+        [HttpGet]
+        public List<User> Get()
+        {
+            var user = _context.User.ToList();
+            return user;
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return _context.User.Select(x => x.Id == id).ToString();
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public void Post(User model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.User.Add(model);
+                    _context.SaveChanges();
+                }
+            }catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
