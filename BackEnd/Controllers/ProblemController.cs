a93e1a82bca3be1cb48e924a38577a824ca1ed44
Neda <neda.ramanauskaite@ktu.edu>
2024-02-23 17:31:34 +0200

    add project

---
+++ BackEnd/Controllers/ProblemController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using BackEnd.Data;
+using BackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEnd.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProblemController : ControllerBase
+    {
+        
+        private readonly LithubContext _context;
+        public ProblemController(LithubContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<UserController>
+        [HttpGet]
+        public List<Problem> Get()
+        {
+            var problem = _context.Problem.ToList();
+            return problem;
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return _context.Problem.Select(x => x.Id == id).ToString();
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public void Post(Problem model)
+        {
+            Console.WriteLine("njnj");
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Problem.Add(model);
+                    _context.SaveChanges();
+                }
+            }catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- lithub/src/addProject.tsx
+++ lithub/src/addProject.tsx
@@ -1,76 +1,58 @@
-import React, {useEffect, useState} from 'react';
-        import { TextField } from "@mui/material";
-        import { Link } from "react-router-dom";
-        import axios, {AxiosResponse} from "axios";
+import React, {useState} from 'react';
+import { TextField } from "@mui/material";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import dayjs from "dayjs";
 
-interface problemData{
-        id: number;
-        title:string;
-        description:string;
-        langueges:string;
-        link:string;
-        lastUpdate:Date;
-}
 function getDate() {
         const today = new Date();
-        const month = today.getMonth() + 1;
-        const year = today.getFullYear();
-        const date = today.getDate();
-        return `${year}-${month}-${date}`;
+        return dayjs(today).format("YYYY-MM-DD");
 }
 function Project() {
-        return (<div>
-<Display/>
-</div>
-        );
+    console.log('start');
+    const [formData, setFormData] = useState({
+        id: 0,
+        title: "string",
+        description: "string",
+        lastUpdate: getDate(),
+        languages: "string",
+        link: "string"
+    });
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('https://localhost:7054/api/Problem', {
+                ...formData,
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            console.log('Response from server:', response.data);
+            //navigate('/');
+            window.location.reload()
+        } catch (error) {
+            console.error('Error submitting post:', error);
         }
-function Display(){
-        const [formData, setFormData] = useState({
-        Id:0,
-        Title: '',
-        description:'',
-        langueges:'',
-        link:'',
-        lastUpdate:getDate()
-        });
-        const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-const { name, value } = e.target;
-setFormData({ ...formData, [name]: value });
-};
-const [problem, setProblem] = useState<problemData[]>([]);
-
-const handleSubmit = async (e: React.FormEvent) => {
-e.preventDefault();
-
-try {
-const response = await axios.post('https://localhost:7054/api/Problem', {
-...formData,
-}, {
-headers: {
-'Content-Type': 'application/json',
-},
-});
-console.log('Response from server:', response.data);
-//navigate('/');
-window.location.reload()
-} catch (error) {
-console.error('Error submitting post:', error);
+    };
+    return(
+        <div>
+            <center><h1>Naujas projektas</h1></center>
+            <p>Pavadinimas:   {' '} <TextField onChange={handleInputChange} name="title"/></p>
+            <p>Aprašymas: {' '} <TextField multiline = {true} onChange={handleInputChange} name="description"/></p>
+            <p>Kalbos: {' '} <TextField onChange={handleInputChange} name="languages"/></p>
+            <p>GitHub nuoroda: {' '} <TextField onChange={handleInputChange} name="link"/></p>
+            <button type="submit" onClick={handleSubmit}>Išsaugoti</button>
+        </div>
+    );
 }
-};
-return(
-<div>
-<center><h1>New project</h1></center>
-<p>Title:   {' '} <TextField onChange={handleInputChange} name="Title"/></p>
-<p>About: {' '}
-    <TextField multiline = {true} onChange={handleInputChange} name="Description"/></p>
-<p>Languages: {' '}
-    <TextField onChange={handleInputChange} name="Languages"/></p>
-<p>GitHub link: {' '}
-    <TextField onChange={handleInputChange} name="Link"/></p>
-    <Link to="/project">
-        <button type="submit" onClick={handleSubmit}>Submit</button>
-</Link>
-</div>
-        );
-}
-        export default Project;
+
+
+export default Project;
