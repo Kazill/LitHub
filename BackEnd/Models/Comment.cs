3a9f5a6ae35b1186179c0488f7a300baf3599000
Kazill <124060943+Kazill@users.noreply.github.com>
2024-03-13 13:33:12 +0200

    Add files via upload

---
+++ BackEnd/Models/Comment.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackEnd.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        public DateTime PostedDate { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey("Id")]
+        public int ProblemId { get; set; }
+
+        [ForeignKey("Id")]
+        public int? ParentCommentId { get; set; }
+
+
+        public virtual ICollection<Comment> Replies { get; set; } = new List<Comment>();
+    }
+}
